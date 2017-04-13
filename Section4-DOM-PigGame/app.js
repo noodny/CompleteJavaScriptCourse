9b8b4cc75915210aec7f9b16a93199fb598b9d22
Krzysztof Bury <krzysiek.bury@gmail.com>
2017-04-13 23:40:08 +0200

    Pig Game app.js changes simple logic according to course.

--- Section4-DOM-PigGame/app.js
+++ Section4-DOM-PigGame/app.js
@@ -8,3 +8,34 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScores, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+/* Testing
+Examples of GETter
+var x = document.querySelector('#score=0').textContent; 
+
+console.log(x);
+
+Hidding the dice at the beginning
+document.querySelector('.dice').style.display = 'none';
+*/
+
+
+
+
+
+
+
+
+
+
+
