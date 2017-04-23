e7b73e4745a7cc2da8d7a316e7cdf2b9d35c43e5
Krzysztof Bury <krzysiek.bury@gmail.com>
2017-04-23 10:22:18 +0200

    Changes to app.js Pig Game

--- Section4-DOM-PigGame/app.js
+++ Section4-DOM-PigGame/app.js
@@ -15,17 +15,23 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
+/* Testing
 document.querySelector('#current-' + activePlayer).textContent = dice;
 
-/* Testing
 Examples of GETter
 var x = document.querySelector('#score=0').textContent; 
-
 console.log(x);
 
 Hidding the dice at the beginning
-document.querySelector('.dice').style.display = 'none';
+
 */
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	
@@ -33,13 +39,52 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	var dice = Math.floor(Math.random() * 6) + 1;
 
 	//2. Display the result
-	
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
 
 	//3. Update the round score IF the rolled number was NOT a 1
-	
-	
+	if (dice !== 1) {
+		//Add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;		
+	} else {
+		nextPlayer();
+	}	
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	//1. Add CURRENT score to GLOBAL score
+	scores[activePlayer] += roundScore;
+
+	//2. Update UI 
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	//3. Check if player won the game
+	if (scores[activePlayer] >= 10) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+		//Next Player 
+		nextPlayer();
+	}
+});
+
+function nextPlayer() {
+	//Next player
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+	
+	document.querySelector('.dice').style.display = 'none';
+};
 
 // Callback function - when a function even is triggering another function
 // to execute 
