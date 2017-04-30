d474e349611905ec64dd86755a01956ae3cc735e
Krzysztof Bury <krzysiek.bury@gmail.com>
2017-04-30 13:15:48 +0200

    PigGame Coding Challenge

--- CodingChallenges/CodingChallengePigGame.js
+++ CodingChallenges/CodingChallengePigGame.js
@@ -9,28 +9,49 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, gamePlaying;
+var scores, roundScores, activePlayer, gamePlaying, lastDice;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	if (gamePlaying) {
 		// 1. Random number
-		var dice = Math.floor(Math.random() * 6) + 1;
+		var dice1 = Math.floor(Math.random() * 6) + 1;
+		var dice2 = Math.floor(Math.random() * 6) + 1;
 
 		//2. Display the result
-		var diceDOM = document.querySelector('.dice');
-		diceDOM.style.display = 'block';
-		diceDOM.src = 'dice-' + dice + '.png';
+		document.getElementById('dice-1').style.display = 'block';
+		document.getElementById('dice-2').style.display = 'block';
+		document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+		document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
 		//3. Update the round score IF the rolled number was NOT a 1
-		if (dice !== 1) {
+		if (dice1 !== 1 && dice2 !== 1) {
+			//Add score
+			roundScore += dice1 + dice2;
+			document.querySelector('#current-' + activePlayer).textContent = roundScore;		
+		} else {
+			nextPlayer();
+		}
+
+		lastDice = dice1 + dice2;
+		/*
+		if (dice === 6 && lastDice ===6) {
+			//Player looses score
+			scores[activePlayer] = 0;
+			document.querySelector('#score-' + activePlayer).textContent = '0';
+			nextPlayer();
+		} else if (dice !== 1) {
 			//Add score
 			roundScore += dice;
 			document.querySelector('#current-' + activePlayer).textContent = roundScore;		
 		} else {
 			nextPlayer();
 		}	
+
+		lastDice = dice;
+		*/
+	
 	}
 });
 
@@ -42,10 +63,22 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		//2. Update UI 
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+		var input = document.querySelector('.final-score').value;
+		var winningScore;
+
+		// Undefined, 0, null or "" are COERCED to false 
+		// Anything else is COERCED to true
+		if (input) {
+			winningScore = input;
+		} else {
+			winningScore = 100;
+		}
+
 		//3. Check if player won the game
-		if (scores[activePlayer] >= 100) {
-			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-			document.querySelector('.dice').style.display = 'none';
+		if (scores[activePlayer] >= winningScore) {
+			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';			
+			document.getElementById('dice-1').style.display = 'none';
+			document.getElementById('dice-2').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 			gamePlaying = false;
@@ -76,7 +109,8 @@ function init() {
 	roundScore = 0;
 	gamePlaying = true;
 
-	document.querySelector('.dice').style.display = 'none';
+	document.getElementById('dice-1').style.display = 'none';
+	document.getElementById('dice-2').style.display = 'none';	
 
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
@@ -95,6 +129,8 @@ function init() {
 	document.querySelector('.player-0-panel').classList.add('active');	
 };
 
+
+
 // Callback function - when a function even is triggering another function
 // to execute 
 
@@ -103,3 +139,4 @@ function init() {
 	// Function used insinde of a callback that doesn't have a name is called 
 	// Anonymous function
 });*/
+
